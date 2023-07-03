4427d873a365f4f65b05024b1ecb12e35730f366
MerooMong <powerhyn@gmail.com>
2023-07-03 20:10:08 +0900

    오타 수정

--- deploy-script-dev/deploy-before.sh
+++ deploy-script-dev/deploy-before.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "> check pid"
-CURRENT_PID=$(pgrep -f jenkins-deploy-api)
+CURRENT_PID=$(pgrep -f jenkins-deploy-api-dev)
 echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
     echo "> service no running"
 else
     echo "> stop service "
-    sudo service jenkins-deploy-api stop
+    sudo service jenkins-deploy-api-dev stop
 fi
